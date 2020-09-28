7bbdf2de362a45e9d9b4732fec3d44042ce9b6e5
Emily Vogel <eav990@gmail.com>
2020-09-28 19:25:01 -0400

    Rename home to notifications

--- app/src/main/java/com/example/final_project/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/final_project/ui/notifications/NotificationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.final_project.ui.home
+package com.example.final_project.ui.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Notification recycler view"
     }
     val text: LiveData<String> = _text
 }
