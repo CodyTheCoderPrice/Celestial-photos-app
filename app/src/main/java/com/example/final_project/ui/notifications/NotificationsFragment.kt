77a7c0c80cd1a8e4e03233c608624e5db24fb130
Emily Vogel <eav990@gmail.com>
2020-10-07 14:38:31 -0400

    Create recycler view

--- app/src/main/java/com/example/final_project/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/final_project/ui/notifications/NotificationsFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.final_project.R
+import com.example.final_project.model.Notification
+
 
 class NotificationsFragment : Fragment() {
 
@@ -22,10 +25,21 @@ class NotificationsFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
+        val list = listOf(
+            Notification("A", "1", "https://google.com", "12345", "Hello World"),
+            Notification("AS", "1", "https://google.com", "12345", "Hello World"),
+            Notification("ASD", "1", "https://google.com", "12345", "Hello World"),
+            Notification("ASDF", "1", "https://google.com", "12345", "Hello World")
+        )
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.notificationRV)
+        val layoutManager =  LinearLayoutManager(root.context)
+        val dividerItemDecoration = DividerItemDecoration(recyclerView.context, layoutManager.orientation)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = NotificationRecyclerAdapter(list)
+        recyclerView.addItemDecoration(dividerItemDecoration)
         return root
     }
 }
