50d4694e05ea005271255d41ff8d3c855be079b5
Cody Price <CodyWilliamPrice@outlook.com>
2020-10-12 13:43:16 -0400

    Video functionality needs fixing

--- app/src/main/java/com/example/final_project/APOD.kt
+++ app/src/main/java/com/example/final_project/APOD.kt
@@ -1,23 +1,20 @@
 package com.example.final_project
 
 import android.content.Context
-import android.graphics.drawable.Drawable
-import android.icu.number.NumberFormatter.with
-import android.icu.number.NumberRangeFormatter.with
-import android.util.Log
+import android.content.Intent
+import android.net.Uri
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
-import java.io.InputStream
-import java.net.URL
 
 
 class APOD {
-    fun getAPOD(context: Context?, textView: TextView, imageView: ImageView) {
+    fun getAPOD(context: Context, textView: TextView, imageView: ImageView) {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(context)
         val url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
@@ -28,12 +25,18 @@ class APOD {
             Request.Method.GET, url,
             { response ->
                 val apodObject: ApodObject = gson.fromJson(response, ApodObject::class.java)
-                Log.i("Here", apodObject.url)
                 textView.text = apodObject.url
 
-                if (apodObject.media_type == "image") {
-                    Picasso.with(context).load(apodObject.url).into(imageView);
-                }
+//                if (apodObject.media_type == "image") {
+//                    Picasso.with(context).load(apodObject.url).into(imageView);
+//                } else if (apodObject.media_type == "video") {
+                
+                    startActivity(context, Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/")), null)
+
+                    //val intent: Intent = Intent(Intent.ACTION_VIEW)
+                    //intent.setDataAndType(Uri.parse("https://www.youtube.com/watch?v=DzrCEm1ZBRY"), "video/*")
+                    //startActivity(context, Intent.createChooser(intent, "Complete action using"), null)
+                //}
 
             },
             { textView.text = "Request didn't work" })
--- app/src/main/java/com/example/final_project/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/final_project/ui/gallery/GalleryFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.final_project.APOD
+import com.example.final_project.ApodObject
 import com.example.final_project.R
 
 class GalleryFragment : Fragment() {
@@ -17,18 +14,19 @@ class GalleryFragment : Fragment() {
     private lateinit var galleryViewModel: GalleryViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.url_header_tv)
+
+        /*val textView: TextView = root.findViewById(R.id.url_header_tv)
         val imageView: ImageView = root.findViewById(R.id.apod_IV)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            APOD().getAPOD(activity?.applicationContext, textView, imageView)
-        })
+            activity?.applicationContext?.let { it1 -> APOD().getAPOD(it1, textView, imageView) }
+        })*/
         return root
     }
 }
